410ca752dee39deb2204fa0c0eea3ae8325b6d84
coreone <coreone@users.noreply.github.com>
2024-01-05 13:56:37 -0500

    feat: move to using a pre-built module (#24)
    fix: linting file updates
    fix: pre-commit version updates
    feat: move Actions to shared workflows
    fix: update provider version

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,31 @@
-data "google_project" "project" {}
+resource "google_project" "default" {
+  auto_create_network = false
+  billing_account     = var.billing_account
+  folder_id           = var.folder_id
+  labels              = { billing = lower(var.billing_account), costobject = "broad-${var.cost_object}" }
+  name                = var.project_name
+  project_id          = var.project
+  skip_delete         = true
+}
+
+resource "google_resource_manager_lien" "default" {
+  parent       = "projects/${google_project.default.number}"
+  restrictions = ["resourcemanager.projects.delete"]
+  origin       = "${google_project.default.project_id}-project-lien-protection"
+  reason       = "This is the primary project for Broad domain redirects and should never be deleted"
+}
+
+resource "google_project_service" "api_services" {
+  for_each = toset(var.api_services)
+  service = each.key
+
+  disable_dependent_services = false
+  disable_on_destroy = false
+
+  depends_on = [
+    google_project.default
+  ]
+}
 
 resource "google_project_iam_policy" "broad_redirect" {
   policy_data = data.google_iam_policy.admin.policy_data
@@ -6,11 +33,10 @@ resource "google_project_iam_policy" "broad_redirect" {
 }
 
 data "google_iam_policy" "admin" {
-  # binding {
-  #   members = [
-  #     "group:operations@broadinstitute.org",
-  #   ]
-  #   role = "roles/owner"
-  # }
-
+  binding {
+    members = [
+      "group:devnull-sa@broadinstitute.org",
+    ]
+    role = "roles/owner"
+  }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,33 @@
+variable "api_services" {
+  default = [
+    "compute.googleapis.com",
+  ]
+  description = "The billing account used by the project"
+  type        = list
+}
+
+variable "billing_account" {
+  description = "The billing account used by the project"
+  type        = string
+}
+
+variable "cost_object" {
+  description = "The cost object that pays for the project"
+  type        = string
+}
+
+variable "folder_id" {
+  description = "The ID for the folder in which the project should reside"
+  type        = string
+}
+
 variable "project" {
-  description = "The deployment project"
+  description = "The project ID used for this project"
+  type        = string
+}
+
+variable "project_name" {
+  description = "The descriptive name for the project"
   type        = string
 }
 
